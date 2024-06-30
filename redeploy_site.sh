f0691ae66abe0ac629cb19fe32f295c42a345bdd
YeyoM <yeyomoreno2003@hotmail.com>
2024-06-30 17:04:29 -0600

    fix: :bug: debugging and fixing ci and cd pipelines

--- redeploy_site.sh
+++ redeploy_site.sh
@@ -9,15 +9,25 @@ echo "Updating git repo"
 git fetch && git reset origin/main --hard 
 
 # Update the script
-rm /root/redeploy_site.sh 
+echo "Updating redeploy_site.sh"
+rm /root/redeploy_site.sh -f
 cp redeploy_site.sh /root/redeploy_site.sh
 chmod +x /root/redeploy_site.sh
 
+echo "Updated redeploy_site.sh"
+ls -l /root/redeploy_site.sh
+cat /root/redeploy_site.sh
+
 # Delete the old containers
+echo "Deleting old containers"
 docker compose -f docker-compose.prod.yml down
 
 # Delete all docker volumes
-docker system prune -a --volumes
+echo "Deleting all docker volumes"
+docker system prune -a --volumes -f
 
 # Build the new containers
+echo "Building new containers"
 docker compose -f docker-compose.prod.yml up -d --build
+
+echo "Exiting project"
